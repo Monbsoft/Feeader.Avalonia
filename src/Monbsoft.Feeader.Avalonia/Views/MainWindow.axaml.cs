0b12a96e6538f73c84289c7847bfa19351806c86
oliver254 <oliver254@hotmail.fr>
2022-05-14 11:34:36 +0200

    Ajoutez des fichiers projet.

---
+++ src/Monbsoft.Feeader.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Monbsoft.Feeader.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
